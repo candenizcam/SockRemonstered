14610c24e29fa40ce0b647a91c6d6e6841038441
candenizcam <e193777@metu.edu.tr>
2022-08-21 20:10:26 +0300

    test stuff

--- Assets/Scenes/TestMain.cs
+++ Assets/Scenes/TestMain.cs
@@ -4,12 +4,71 @@ using UnityEngine;
 
 public class TestMain : MonoBehaviour
 {
+    public GameObject visual;
+    public GameObject otherVisual;
+    public GameObject otherOtherVisual; 
+    public GameObject otherOtherOtherVisual;
+
+    public GameObject otherVisual5;
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log($"screen, width {Screen.width}, height {Screen.height}");
         
+        
+        
+        // Debug.Log($"camera main rect width: {Camera.main.rect.width}, height: {Camera.main.rect.height}"); 1,1
+        
+        Debug.Log($"camera main pixel width: {Camera.main.pixelWidth}, height: {Camera.main.pixelHeight}");
+        
+        //Debug.Log($"camera main target width: {Camera.main.targetTexture.width}, height: {Camera.main.targetTexture.height}");
+        
+        Debug.Log($"camera main target width: {Camera.main.scaledPixelWidth}, height: {Camera.main.scaledPixelHeight}");
+        
+        // Debug.Log($"Camera.main.transform.lossyScale x: {Camera.main.transform.lossyScale.x}, y: {Camera.main.transform.lossyScale.y}"); 1,1
+        
+        Debug.Log($"v2 screen world {Camera.main.ViewportToScreenPoint(new Vector3(1,1,0))} {Camera.main.ViewportToWorldPoint(new Vector3(1,1,0))}");
+
+        var gc1 = visual.GetComponent<SpriteRenderer>();
+        rendererStats("1",gc1);
+       
+        var gc5 = otherVisual5.GetComponent<SpriteRenderer>();
+        rendererStats("5",gc5);
+
+        Debug.Log($"orth size {Camera.main.orthographicSize}");
+        
+        Debug.Log($"v2 screen world {Camera.main.ViewportToScreenPoint(new Vector3(1,1,0))} {Camera.main.ViewportToWorldPoint(new Vector3(1,1,0))}");
+        
+        Camera.main.orthographicSize *= 2f;
+
+        
+        var gc12 = visual.GetComponent<SpriteRenderer>();
+        rendererStats("12",gc12);
+       
+        var gc52 = otherVisual5.GetComponent<SpriteRenderer>();
+        rendererStats("52",gc52);
+        
+        Debug.Log($"orth size {Camera.main.orthographicSize}");
+        
+        Debug.Log($"v2 screen world {Camera.main.ViewportToScreenPoint(new Vector3(1,1,0))} {Camera.main.ViewportToWorldPoint(new Vector3(1,1,0))}");
+
+        visual.transform.localScale = new Vector3(2f, 2f, 1f);
+        rendererStats("12",gc12);
+       
+        otherVisual5.transform.localScale = new Vector3(2f, 2f, 1f);
+        rendererStats("52",gc52);
     }
 
+    void rendererStats(string pre, SpriteRenderer sr)
+    {
+        var bounds = sr.sprite.bounds;
+        Debug.Log($"{pre} size .x: {sr.size.x}, .y: {sr.size.y}, {sr.transform.lossyScale.x}, {sr.transform.lossyScale.y}");
+        
+        
+        //Debug.Log($"{pre} sprite rect width: {sr.sprite.rect.width}, height: {sr.sprite.rect.height}");
+    }
+    
+    
     // Update is called once per frame
     void Update()
     {
