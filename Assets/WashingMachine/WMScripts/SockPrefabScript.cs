9f397d59bc07c44848b8bcac4de4e19ba13e047f
candenizcam <e193777@metu.edu.tr>
2022-08-22 17:38:42 +0300

    sock random generation implemented

--- Assets/WashingMachine/WMScripts/SockPrefabScript.cs
+++ Assets/WashingMachine/WMScripts/SockPrefabScript.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.U2D;
+using Random = System.Random;
 
 public class SockPrefabScript : MonoBehaviour
 {
     public List<SpriteRenderer> sockSpriteList;
     public Collider2D hitbox;
     public SpriteShapeRenderer hitboxRenderer;
-    public SpriteShapeController hitboxController;
+    
     // Start is called before the first frame update
     public bool ToBeDestroyed = false;
+
+
+    private int _selectedSprite;
     void Awake()
     {
-        //hitboxRenderer.color = Color.clear;
+        hitboxRenderer.color = Color.clear;
+
+        var r = new Random();
+
+        _selectedSprite = r.Next(sockSpriteList.Count);
+        for (int i = 0; i < sockSpriteList.Count; i++)
+        {
+            sockSpriteList[i].gameObject.SetActive(i == _selectedSprite);
+        }
+
     }
 
     // Update is called once per frame
