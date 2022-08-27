e665366f62f90ceba1040ccfdd69f1477d79155a
candenizcam <e193777@metu.edu.tr>
2022-08-27 13:22:38 +0300

    card stuff

--- Assets/Cards/CScripts/CardsMain.cs
+++ Assets/Cards/CScripts/CardsMain.cs
@@ -24,17 +24,24 @@ public class CardsMain : MonoBehaviour
         
         _mainCamera = new CardLayout(Camera.main);
 
-        _rows = 5;
-        _columns = 4;
+        _rows = 4;
+        _columns = 6;
 
 
         _sockCardPrefab = Resources.Load("prefabs/SockCardPrefab");
         var ssp1 = _sockCardPrefab.GetComponent<SockCardPrefabScript>();
 
+        var cardList = generateCardList(ssp1.socks.Count);
+
 
         //var r = new Range(0, ssp1.socks.Count);
 
 
+
+
+
+
+        /*
         var f = (_rows * _columns) / ssp1.socks.Count/2f;
         
         var cardTypes = new List<int>();
@@ -50,10 +57,41 @@ public class CardsMain : MonoBehaviour
             cardTypes.AddRange(m);
             
         }
+        */
 
 
-        Debug.Log($"ssp1: {cardTypes.Count*2}");
+        // Debug.Log($"ssp1: {cardTypes.Count*2}");
 
+    }
+
+
+    private List<int> generateCardList(int socksCount)
+    {
+        var totalCards =( _rows * _columns)/2;
+        if ((_rows * _columns) % 2 != 0)
+        {
+            throw new Exception("invalid row col entry");
+        }
+        var fullTurns = totalCards / socksCount;
+        var missing = totalCards % socksCount;
+        var r = new List<int>();
+        for (int j = 0; j <socksCount; j++)
+        {
+            r.Add(j);
+        }
+        var m = r.OrderBy(_ => _random.Next()).ToList();
+        var extra = m.Take(missing);
+
+        var r2 = new List<int>();
+        for (int i = 0; i < fullTurns; i++)
+        {
+            for (int j = 0; j < socksCount; j++)
+            {
+                r2.Add(j);
+            }
+        }
+        r2.AddRange(extra);
+        return r2;
     }
 
     // Update is called once per frame
