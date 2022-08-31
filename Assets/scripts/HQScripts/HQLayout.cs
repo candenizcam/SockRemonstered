7390878eac652eb7a9dcf7eec83d46bfac62bcdf
candenizcam <e193777@metu.edu.tr>
2022-08-31 16:10:45 +0300

    scripts reorganized

---
+++ Assets/scripts/HQScripts/HQLayout.cs
@@ -0,0 +1,13 @@
+﻿using Classes;
+using UnityEngine;
+
+namespace HQScripts
+{
+    public class HQLayout: GameLayout
+    {
+        public HQLayout(Camera c) : base(c)
+        {
+            
+        }
+    }
+}
